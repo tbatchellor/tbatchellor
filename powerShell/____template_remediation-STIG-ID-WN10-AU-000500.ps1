87977333ea7c1ee3a011fd69bc318e68637a90e8
Tom Batchellor <75383149+tbatchellor@users.noreply.github.com>
2025-08-23 10:17:32 -0400

    Create ____template_remediation-STIG-ID-WN10-AU-000500.ps1

---
+++ powerShell/____template_remediation-STIG-ID-WN10-AU-000500.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Tom Batchellor
+    LinkedIn        : linkedin.com/in/batchellorTom/
+    GitHub          : github.com/tBatchellor
+    Date Created    : 2025-01-09
+    Last Modified   : 2025-01-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
